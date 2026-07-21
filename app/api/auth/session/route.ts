ebbdf1370d64b34c30496cb4a4040da34e053bc8
lida Gholamian <ta.gholamian@gmail.com>
2026-07-21 11:32:04 +0330

    solve session error

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,15 +1,18 @@
 import { decryptSession } from "@/app/utils/session";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET() {
     const cookieStore = await cookies();
     const encryptedSession = cookieStore.get('clb-session')?.value;
 
-    if (!encryptedSession){
-        return Response.json(null, {status: 401})
+    if (!encryptedSession) {
+        return NextResponse.json({ error: 'Session not found' }, {
+            status: 400
+        });
     }
 
     const session = await decryptSession(encryptedSession);
-    return Response.json(session);
+    return NextResponse.json(session);
 
 }
